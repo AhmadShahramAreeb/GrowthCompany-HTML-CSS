3c5b6f50c04dd3fc1a8bdd48662c72a7399be792
Ahmad Shahram Areeb <shahram.1732@gmail.com>
2024-08-16 17:18:44 +0300

    responsive CSS codes Added
    Middle size and mobile screen responsive codes added for the Home page.

---
+++ js/main.js
@@ -0,0 +1,11 @@
+//for mobile menu
+document.addEventListener("DOMContentLoaded", () => {
+  const hamburgerButton = document.querySelector(".hamburger-button");
+  const mobileMenu = document.querySelector(".mobile-menu");
+
+  /*The toggle method is used to add the active class to the
+   mobileMenu element if it doesn’t already have it, or remove it if it does.*/
+  hamburgerButton.addEventListener("click", () => {
+    mobileMenu.classList.toggle("active");
+  });
+});
